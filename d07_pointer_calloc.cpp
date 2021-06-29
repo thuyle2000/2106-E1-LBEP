848e875a624e2ce60ed811d769a744259d89ff25
Le Mong Thuy <thuyle2000@gmail.com>
2021-06-29 20:40:00 +0700

    Upload June  29, 2021

---
+++ d07_pointer_calloc.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main() {
+	int *p, n;
+
+	while(1) {
+		printf(" vui long nhap so phan tu cua day so nguyen [5-20]: ");
+		scanf("%d", &n);
+		if(n>=5 && n<=20) {
+			break; //thoat vong lap
+		}
+	}
+	
+	
+	
+	p = (int *)calloc(n,sizeof(int) );
+	
+	if(p!=NULL){
+		
+		srand(time(0));
+		
+		//tao n-so ngau nhien 
+		for(int i=0; i<n; i++){
+			*(p+i) = 20 + rand()%180;
+		}
+		
+		printf("\n\n >> Day so ngau nhien vua tao: \n");
+		for(int i=0; i <n; i++){
+			printf("%d ", *(p+i) );
+		}
+		
+		//sap xep lai day so nguyen theo thu tu tang dan
+		int temp = 0;
+		for(int i=0; i<n-1; i++){
+			for(int k=i+1; k<n; k++){
+				if(  *(p+i) > *(p+k)  ){
+					temp = *(p+i);
+					*(p+i) = *(p+k);
+					*(p+k)= temp;
+				}
+			}
+		}
+		
+		printf("\n\n >> Day so sau khi xep thu tu: \n");
+		for(int i=0; i <n; i++){
+			printf("%d ", *(p+i));
+		}		
+		
+		
+		free(p); //tra vung nho xin cap phat ve lai cho he thong
+		
+	}
+	else{
+		printf("HT ko du vung nho cap phat theo yeu cau !");
+	}
+
+}
+
